38a57f3aa52b8621a857f0d3badd936d962c8a3d
Lindsey Bieda <hazardous.narayan@gmail.com>
2015-12-12 12:53:07 -0500

    Download and display highscores from highscore server

---
+++ Assets/Scripts/Highscore Scene/GetHighscores.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Net;
+using SimpleJSON;
+using UnityEngine.UI;
+
+public class GetHighscores : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        var json = new WebClient().DownloadString("http://47bf6273.ngrok.com/highscores.json");
+        var result = JSON.Parse(json);
+        string highscores = "";
+
+        for(int i=0; i<10; i++) {
+            highscores += string.Format("{0,-2:00}. {1,8} {2,10}\n", (i+1), result[i]["name"].Value.ToUpper(), result[i]["score"].Value);
+        }
+
+        gameObject.GetComponent<Text>().text = highscores;
+    }
+}
