7fac76c8cac26f6f05da891a7d8d5b223bf7bc6d
Lindsey Bieda <hazardous.narayan@gmail.com>
2015-12-12 14:12:45 -0500

    Adding submit score code

---
+++ Assets/Scripts/Main Scene Scripts/SubmitHighscore.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Net;
+
+public class SubmitHighscore : MonoBehaviour {
+    void Submit(string name, int score) {
+        WebRequest request = WebRequest.Create("http://47bf6273.ngrok.com/add/" + name + "/score/" + score + "/");
+        request.Method = "POST";
+        request.GetResponse();
+    }
+}
