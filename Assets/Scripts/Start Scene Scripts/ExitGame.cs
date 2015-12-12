c4dd67c1d964846dd0c4a814e43c2279d297244e
Lindsey Bieda <hazardous.narayan@gmail.com>
2015-12-12 10:40:33 -0500

    Adding exit game button ability

---
+++ Assets/Scripts/Start Scene Scripts/ExitGame.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExitGame : MonoBehaviour {
+    void Update() {
+        if (Input.GetKey("escape") || Input.GetKey(KeyCode.Q)) {
+            Exit();
+        }
+    }
+
+    void Exit() {
+        Application.Quit();
+    }
+}
