d2d97849b84cb8cabf19e2c9527ad5a8b83af077
Lindsey Bieda <hazardous.narayan@gmail.com>
2015-12-12 17:43:29 -0500

    Adding super dancy lasers

---
+++ Assets/Scripts/Main Scene Scripts/RotateLaser.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateLaser : MonoBehaviour {
+    [SerializeField] float maxRotation;
+    [SerializeField] float minRotation;
+
+    float rotationSpeed = 10.0f;
+    float multiplier = 1;
+	
+	void Update () {
+        Vector3 rot = gameObject.transform.rotation.eulerAngles;
+
+        if (rot.z <= minRotation) {
+            multiplier = 1;
+        }
+
+        if (rot.z >= maxRotation) {
+            multiplier = -1;
+        }
+
+        rot.z = rot.z + ( multiplier * rotationSpeed * Time.deltaTime);
+
+        gameObject.transform.eulerAngles = rot;
+	}
+}
