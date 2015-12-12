624ee6377c7612424c7267b8b54f6d5231c3c61b
Lindsey Bieda <hazardous.narayan@gmail.com>
2015-12-12 18:47:38 -0500

    Adding speaker animations via scaling

---
+++ Assets/Scripts/Main Scene Scripts/SpeakerThump.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpeakerThump : MonoBehaviour {
+    [SerializeField] float seconds;
+    [SerializeField] float bigScale;
+
+    float smallScale = 1f;
+    float currentScale;
+    float speed = 0.5f;
+
+	void Start () {
+        StartCoroutine("ThumpScale");
+	}
+
+    IEnumerator ThumpScale() {
+        while (true) {
+            currentScale = bigScale;
+            gameObject.transform.localScale = Vector3.one * bigScale;
+
+            yield return new WaitForSeconds(seconds);
+
+            while (currentScale > smallScale) {
+                currentScale -= (Time.deltaTime * speed);
+                yield return new WaitForSeconds(0.1f);
+                gameObject.transform.localScale = Vector3.one * currentScale;
+            }
+        }
+    }
+}
