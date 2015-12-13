3899ee07a7b732f74aa246b002ed0cc943a5a6b9
Lindsey Bieda <hazardous.narayan@gmail.com>
2015-12-13 14:40:44 -0500

    Temporarily hide the rejected kitties ToT

--- Assets/Scripts/Main Scene Scripts/NopePaw.cs
+++ Assets/Scripts/Main Scene Scripts/NopePaw.cs
@@ -3,8 +3,20 @@ using System.Collections;
 
 public class NopePaw : MonoBehaviour {
 	void Update () {
-	    if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.LeftArrow)) {
-            gameObject.GetComponent<Animator>().SetTrigger("nopeTrigger");
+        if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.LeftArrow)) {
+            GameObject currentCat;
+            if (currentCat = GameObject.FindWithTag("CurrentCat")) {
+                gameObject.GetComponent<Animator>().SetTrigger("nopeTrigger");
+                currentCat.tag = "Untagged";
+
+                currentCat.SetActive(false); // temporary
+
+                if (GameObject.FindWithTag("CurrentCat") == null) {
+                    currentCat = Instantiate(currentCat);
+                    currentCat.SetActive(true); // temporary
+                    currentCat.tag = "CurrentCat";
+                }
+            }
         }
 	}
 }
