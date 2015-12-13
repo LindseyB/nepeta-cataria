79b86f82c9432a60af9bee3a71f28abfb09a1ba0
Lindsey Bieda <hazardous.narayan@gmail.com>
2015-12-12 21:04:11 -0500

    Updating formatting for highscores

--- Assets/Scripts/Highscore Scene Scripts/GetHighscores.cs
+++ Assets/Scripts/Highscore Scene Scripts/GetHighscores.cs
@@ -13,7 +13,7 @@ public class GetHighscores : MonoBehaviour {
         string highscores = "";
 
         for(int i=0; i<10; i++) {
-            highscores += string.Format("{0,-2:00}. {1,8} {2,10}\n", (i+1), result[i]["name"].Value.ToUpper(), result[i]["score"].Value);
+            highscores += string.Format("{0,-2:00}. {1,5} {2,10:0000000000}\n", (i+1), result[i]["name"].Value.ToUpper(), int.Parse(result[i]["score"].Value));
         }
 
         gameObject.GetComponent<Text>().text = highscores;
