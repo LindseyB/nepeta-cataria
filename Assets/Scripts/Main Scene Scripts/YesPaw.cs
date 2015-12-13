f31e81bcc05392a30b73834d21e540abf2313950
Lindsey Bieda <hazardous.narayan@gmail.com>
2015-12-13 12:52:50 -0500

    Pull in the new cat if need to

--- Assets/Scripts/Main Scene Scripts/YesPaw.cs
+++ Assets/Scripts/Main Scene Scripts/YesPaw.cs
@@ -3,12 +3,18 @@ using System.Collections;
 
 public class YesPaw : MonoBehaviour {
 	void Update () {
-		if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.RightArrow)) {
-			gameObject.GetComponent<Animator>().SetTrigger("yesTrigger");
-            GameObject currentCat = GameObject.FindWithTag("CurrentCat");
-            Instantiate(currentCat);
-            currentCat.GetComponent<Animator>().SetTrigger("moveIntoClubTrigger");
-            currentCat.tag = "Untagged";
-		}
+        if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.RightArrow)) {
+            GameObject currentCat;
+            if (currentCat = GameObject.FindWithTag("CurrentCat")) {
+                gameObject.GetComponent<Animator>().SetTrigger("yesTrigger");
+                currentCat.GetComponent<Animator>().SetTrigger("moveIntoClubTrigger");
+                currentCat.tag = "Untagged";
+
+                if (GameObject.FindWithTag("CurrentCat") == null) {
+                    currentCat = Instantiate(currentCat);
+                    currentCat.tag = "CurrentCat";
+                }
+            }
+        }
 	}
 }
