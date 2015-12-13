6ae0bada1bcb6a87353eeb010771778670b338c5
Lindsey Bieda <hazardous.narayan@gmail.com>
2015-12-13 11:08:34 -0500

    Left or Right arrow also works

--- Assets/Scripts/Main Scene Scripts/NopePaw.cs
+++ Assets/Scripts/Main Scene Scripts/NopePaw.cs
@@ -2,17 +2,8 @@
 using System.Collections;
 
 public class NopePaw : MonoBehaviour {
-
-    int nopeHash = Animator.StringToHash("nopeAnimation");
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
 	void Update () {
-	    if (Input.GetKeyDown(KeyCode.DownArrow)) {
+	    if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.LeftArrow)) {
             gameObject.GetComponent<Animator>().SetTrigger("nopeTrigger");
         }
 	}
--- Assets/Scripts/Main Scene Scripts/YesPaw.cs
+++ Assets/Scripts/Main Scene Scripts/YesPaw.cs
@@ -2,15 +2,8 @@
 using System.Collections;
 
 public class YesPaw : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown(KeyCode.UpArrow)) {
+		if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.RightArrow)) {
 			gameObject.GetComponent<Animator>().SetTrigger("yesTrigger");
 		}
 	}
