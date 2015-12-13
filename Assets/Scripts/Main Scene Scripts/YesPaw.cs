27e753af10c42ba2ccc631b4043a809e825d4157
jackie kircher <kircher.jackie@gmail.com>
2015-12-13 00:29:29 -0500

    you may pass
    threw in the animation and trigger for the "yes" response. this takes
    a little bit longer than the "no" animation so we may want to tweak that
    later to be the same.

---
+++ Assets/Scripts/Main Scene Scripts/YesPaw.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class YesPaw : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.UpArrow)) {
+			gameObject.GetComponent<Animator>().SetTrigger("yesTrigger");
+		}
+	}
+}
