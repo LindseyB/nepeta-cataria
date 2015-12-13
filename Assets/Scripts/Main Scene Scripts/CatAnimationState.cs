2b71fa867f0a95090ebf078a600c89a3503174a5
Lindsey Bieda <hazardous.narayan@gmail.com>
2015-12-13 14:06:44 -0500

    Make the cats walk when they are moving

---
+++ Assets/Scripts/Main Scene Scripts/CatAnimationState.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CatAnimationState : MonoBehaviour {
+    Animator animator;
+
+    void Start() {
+        animator = gameObject.GetComponent<Animator>();
+    }
+
+    public void Walk() {
+        animator.SetTrigger("catMoveTrigger");
+    }
+
+    public void Stand() {
+        animator.SetTrigger("catStopTrigger");
+    }
+}
--- Assets/Scripts/Main Scene Scripts/CurrentCat.cs
+++ Assets/Scripts/Main Scene Scripts/CurrentCat.cs
@@ -4,15 +4,24 @@ using System.Collections;
 public class CurrentCat : MonoBehaviour {
     Animator animator;
     int doneHash = Animator.StringToHash("Done");
+    int moveLineHash = Animator.StringToHash("moveIntoLine");
+    int moveClubHash = Animator.StringToHash("moveIntoClub");
 
 	void Start () {
          // TODO: dynamically set up parts
         animator = gameObject.GetComponent<Animator>();
+        
 	}
 	
 	void Update () {
-        if(animator.GetCurrentAnimatorStateInfo(0).shortNameHash == doneHash) {
+        CatAnimationState catAnimator = gameObject.GetComponentInChildren<CatAnimationState>();
+        if (animator.GetCurrentAnimatorStateInfo(0).shortNameHash == doneHash) {
             Destroy(gameObject);
+        } else if(animator.GetCurrentAnimatorStateInfo(0).shortNameHash == moveLineHash || 
+                  animator.GetCurrentAnimatorStateInfo(0).shortNameHash == moveClubHash) {
+            catAnimator.Walk();
+        } else {
+            catAnimator.Stand();
         }
 	}
 }
