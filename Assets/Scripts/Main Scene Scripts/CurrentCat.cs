8e2074ed1301ecbec444333c9b84e2dafc5e14dd
jackie kircher <kircher.jackie@gmail.com>
2015-12-13 18:32:34 -0500

    randomly choose a "headstyle"
    certain heads are hairstyles so they need to be colored to match the
    cat. well, they don't NEED to but that's the best option.

--- Assets/Scripts/Main Scene Scripts/CurrentCat.cs
+++ Assets/Scripts/Main Scene Scripts/CurrentCat.cs
@@ -9,9 +9,11 @@ public class CurrentCat : MonoBehaviour {
     int moveClubHash = Animator.StringToHash("moveIntoClub");
 
     Sprite[] tails;
+    Sprite[] heads;
 
 	void Start () {
         tails = Resources.LoadAll<Sprite>("Tails");
+        heads = Resources.LoadAll<Sprite>("Heads");
         Concatenate();
         animator = gameObject.GetComponent<Animator>();
         
@@ -37,6 +39,15 @@ public class CurrentCat : MonoBehaviour {
         SpriteRenderer catTail = gameObject.transform.Find("CatTail").GetComponent<SpriteRenderer>();
         catTail.sprite = tails[Random.Range(0, tails.Length-1)];
         catTail.color = color;
+
+        SpriteRenderer catHead = gameObject.transform.Find("CatHead").GetComponent<SpriteRenderer>();
+        catHead.sprite = heads[Random.Range(0, heads.Length-1)];
+        if (catHead.sprite.name == "cat-hair-1" || catHead.sprite.name == "cat-hair-3") {
+            catHead.color = color;
+        } else {
+            catHead.color = Color.white;
+        }
+
         gameObject.transform.Find("CatBase").GetComponent<SpriteRenderer>().color = color;
     }
 }
