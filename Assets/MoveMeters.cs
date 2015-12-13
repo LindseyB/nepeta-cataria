a5e55466654909605a3a66103c51a0d2be91a409
Lindsey Bieda <hazardous.narayan@gmail.com>
2015-12-13 16:52:39 -0500

    Adding meters with some movement

---
+++ Assets/MoveMeters.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveMeters : MonoBehaviour {
+    float height;
+
+    void Start() {
+        StartCoroutine("Move");
+    }
+
+	void Update() {
+        // TODO: do stuff based on score
+	}
+
+    IEnumerator Move() {
+        while (true) {
+            height = Random.Range(0.1f, 4.0f);
+            foreach (LineRenderer lr in gameObject.GetComponentsInChildren<LineRenderer>()) {
+                lr.SetPosition(0, new Vector3(0, height, 0));
+            }
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+}
