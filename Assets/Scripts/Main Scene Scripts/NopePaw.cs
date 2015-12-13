1b362f949c5cdfa3eab1d8726e4f7263d5e2c7d3
jackie kircher <kircher.jackie@gmail.com>
2015-12-12 23:17:27 -0500

    NOPE
    animations for the nopePaw routine. there might be some artifcats of
    unpleasantness left over from my struggles.

---
+++ Assets/Scripts/Main Scene Scripts/NopePaw.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NopePaw : MonoBehaviour {
+
+    int nopeHash = Animator.StringToHash("nopeAnimation");
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetKeyDown(KeyCode.DownArrow)) {
+            gameObject.GetComponent<Animator>().SetTrigger("nopeTrigger");
+        }
+	}
+}
