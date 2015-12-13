d7c99e9593c55feaffe8cc0e6bfb403bd06eb787
Lindsey Bieda <hazardous.narayan@gmail.com>
2015-12-13 12:09:53 -0500

    Delete current cat once done animating

---
+++ Assets/Scripts/Main Scene Scripts/CurrentCat.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CurrentCat : MonoBehaviour {
+    Animator animator;
+    int doneHash = Animator.StringToHash("Done");
+
+	void Start () {
+        animator = gameObject.GetComponent<Animator>();
+	}
+	
+	void Update () {
+        if(animator.GetCurrentAnimatorStateInfo(0).shortNameHash == doneHash) {
+            Destroy(gameObject);
+        }
+	}
+}
