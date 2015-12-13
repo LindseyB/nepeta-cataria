ea76854558413695fdc307e078920f62b4b1d776
Lindsey Bieda <hazardous.narayan@gmail.com>
2015-12-12 21:52:41 -0500

    Use heroku from now on and more generic for loop

--- Assets/Scripts/Highscore Scene Scripts/GetHighscores.cs
+++ Assets/Scripts/Highscore Scene Scripts/GetHighscores.cs
@@ -8,14 +8,16 @@ public class GetHighscores : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        var json = new WebClient().DownloadString("http://6b1fa8b1.ngrok.com/highscores.json");
-        var result = JSON.Parse(json);
+        string json = new WebClient().DownloadString("http://nepeta-cataria-server.herokuapp.com/highscores.json");
+        JSONArray result = JSON.Parse(json) as JSONArray;
         string highscores = "";
 
-        for(int i=0; i<10; i++) {
-            highscores += string.Format("{0,-2:00}. {1,5} {2,10:0000000000}\n", (i+1), result[i]["name"].Value.ToUpper(), int.Parse(result[i]["score"].Value));
+        int n = 1;
+        foreach(JSONNode item in result) {
+            highscores += string.Format("{0,-2:00}. {1,5} {2,10:0000000000}\n", n, item["name"].Value.ToUpper(), int.Parse(item["score"].Value));
+            n++;
         }
-
+   
         gameObject.GetComponent<Text>().text = highscores;
     }
 }
--- Assets/Scripts/Main Scene Scripts/SubmitHighscore.cs
+++ Assets/Scripts/Main Scene Scripts/SubmitHighscore.cs
@@ -8,7 +8,7 @@ public class SubmitHighscore : MonoBehaviour {
         int score = 0; // TODO: replace with actual score
         string name = GameObject.Find("Name").GetComponent<Text>().text;
 
-        WebRequest request = WebRequest.Create("http://6b1fa8b1.ngrok.com/add/" + name + "/score/" + score + "/");
+        WebRequest request = WebRequest.Create("http://nepeta-cataria-server.herokuapp.com/add/" + name + "/score/" + score + "/");
         request.Method = "POST";
         request.GetResponse();
 
