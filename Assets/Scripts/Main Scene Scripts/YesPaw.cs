fea07168a598c4bfd76737368f84cf85fb9551fb
Lindsey Bieda <hazardous.narayan@gmail.com>
2015-12-13 11:35:30 -0500

    Move cat into club on yes

--- Assets/Scripts/Main Scene Scripts/YesPaw.cs
+++ Assets/Scripts/Main Scene Scripts/YesPaw.cs
@@ -5,6 +5,7 @@ public class YesPaw : MonoBehaviour {
 	void Update () {
 		if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.RightArrow)) {
 			gameObject.GetComponent<Animator>().SetTrigger("yesTrigger");
+            GameObject.FindWithTag("CurrentCat").GetComponent<Animator>().SetTrigger("moveIntoClubTrigger");
 		}
 	}
 }
