86ddf4d74216d916e812b9bd818f8a4679bfc69a
jackie kircher <kircher.jackie@gmail.com>
2015-12-13 21:06:49 -0500

    ensure unique VIP rules

--- Assets/Scripts/Main Scene Scripts/VIPList.cs
+++ Assets/Scripts/Main Scene Scripts/VIPList.cs
@@ -1,25 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class VIPList : MonoBehaviour {
 
     private int numberOfRules = 5;
 
-    Sprite[] headThumbnails;
-    Sprite[] tailThumbnails;
-    Sprite[] rules;
+    List<Sprite> headThumbnails;
+    List<Sprite> tailThumbnails;
+    List<Sprite> rules;
 
 	// Use this for initialization
 	void Start () {
-        headThumbnails = Resources.LoadAll<Sprite>("VIPList/Heads");
-        tailThumbnails = Resources.LoadAll<Sprite>("VIPList/Tails");
+        headThumbnails = new List<Sprite>(Resources.LoadAll<Sprite>("VIPList/Heads"));
+        tailThumbnails = new List<Sprite>(Resources.LoadAll<Sprite>("VIPList/Tails"));
         rules = SetVIPRules();
 
-        gameObject.transform.Find("Rule1").GetComponent<SpriteRenderer>().sprite = rules[0];
-        gameObject.transform.Find("Rule2").GetComponent<SpriteRenderer>().sprite = rules[1];
-        gameObject.transform.Find("Rule3").GetComponent<SpriteRenderer>().sprite = rules[2];
-        gameObject.transform.Find("Rule4").GetComponent<SpriteRenderer>().sprite = rules[3];
-        gameObject.transform.Find("Rule5").GetComponent<SpriteRenderer>().sprite = rules[4];
+        for (int i=0; i < numberOfRules; i++) {
+            gameObject.transform.Find("Rule" + i).GetComponent<SpriteRenderer>().sprite = rules[i];
+        }
     }
 	
 	// Update is called once per frame
@@ -27,26 +26,29 @@ public class VIPList : MonoBehaviour {
 	
 	}
 
-    private Sprite[] SetVIPRules() {
-        Sprite[] newRules = new Sprite[numberOfRules];
+    private List<Sprite> SetVIPRules() {
+        List<Sprite> newRules = new List<Sprite>();
 
         for (int i=0; i < numberOfRules; i++) {
             Sprite rule = PickRandomRule();
-            newRules[i] = rule;
+            newRules.Add(rule);
         }
 
         return newRules;
     }
 
     private Sprite PickRandomRule() {
-        Sprite[][] allRules = new Sprite[2][];
-        allRules[0] = headThumbnails;
-        allRules[1] = tailThumbnails;
+        List<List<Sprite>> allRules = new List<List<Sprite>>();
+        allRules.Add(headThumbnails);
+        allRules.Add(tailThumbnails);
 
-        Sprite[] ruleSet = allRules[Random.Range(0, allRules.Length)];
-        Sprite rule = ruleSet[Random.Range(0, ruleSet.Length)];
+        int ruleSetIndex = Random.Range(0, allRules.Count);
+        List<Sprite> ruleSet = allRules[ruleSetIndex];
 
-        Debug.Log(rule.name);
+        int ruleIndex = Random.Range(0, ruleSet.Count);
+        Sprite rule = ruleSet[ruleIndex];
+        ruleSet.RemoveAt(ruleIndex);
+
         return rule;
     }
 }
