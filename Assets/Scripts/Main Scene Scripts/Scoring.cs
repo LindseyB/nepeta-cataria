747ef82dc1578d215803560b4329cec88222735c
Lindsey Bieda <hazardous.narayan@gmail.com>
2015-12-14 10:58:00 -0500

    Starting to add in score item

---
+++ Assets/Scripts/Main Scene Scripts/Scoring.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Scoring : MonoBehaviour {
+    int score = 0;
+    Text[] scoreItems;
+
+	void Start () {
+        scoreItems = gameObject.GetComponentsInChildren<Text>();
+        UpdateScore();
+	}
+
+	void Update () {
+	
+	}
+
+    void UpdateScore() {
+        foreach(Text item in scoreItems) {
+            item.text = score.ToString();
+        }
+    }
+}
